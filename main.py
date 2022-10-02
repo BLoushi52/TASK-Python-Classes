11c50f8cf5a16b16cf6a3664878746a620e3d453
BLoushi52 <k.albloushii@hotmail.com>
2022-10-02 14:45:34 +0300

    solved vendor class question

--- main.py
+++ main.py
@@ -26,16 +26,25 @@ class Person:
         self.location = point
         print(f"New location has been set to {self.location}")
 
-    
-    
-
 
 person = Person("Moh", 5, 50)
-print(person.moveTo(2))   #Testing
+# print(person.moveTo(2))   #Testing
 
 class Vendor:
+    def __init__(self, name, location, money):
+        super().__init__(name, location, money)
+        self.range = 5
+        self.price = 1
+
+    def sellTo(self, customer, number_of_icecreams):
+        customer.location = person.location
+        # moveTo(self,customer.location )
+        customer.wallet.debit(number_of_icecreams * self.price)
+        self.wallet.credit(number_of_icecreams * self.price)
+        
+
     # implement Vendor!
-    pass
+    
 
 
 # vendor = Vendor("Abdallah", 3, 6)
